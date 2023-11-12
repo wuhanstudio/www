fb116e212e646d1053cf184f9d03216be0b9b49c
wuhanstudio <wuhanstudio@qq.com>
2023-11-12 22:52:53 +0000

    [json] Get posts from json files

--- doc/js/scripts.js
+++ doc/js/scripts.js
@@ -1,29 +1,47 @@
-/*!
-* Start Bootstrap - Clean Blog v6.0.9 (https://startbootstrap.com/theme/clean-blog)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
-*/
-window.addEventListener('DOMContentLoaded', () => {
-    let scrollPos = 0;
-    const mainNav = document.getElementById('mainNav');
-    const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
-        const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
-            // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-visible');
-            } else {
-                console.log(123);
-                mainNav.classList.remove('is-visible', 'is-fixed');
+function showPosts(types, home) {
+    var all_posts = [];
+    for (let j = 0; j < types.length; j++){
+        var type = types[j];
+        $.ajax({
+            async: false,
+            type: 'GET',
+            url: type + ".json",
+            success: function(posts) {
+                for (let i = 0; i < posts.length; i++) {
+                    if (posts[i].type === type)
+                        if(home === true){
+                            // Show the post on the homepage
+                            if(!posts[i].home) continue
+                        }
+                        all_posts.push(posts[i])
+                }
             }
-        } else {
-            // Scrolling Down
-            mainNav.classList.remove(['is-visible']);
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-fixed');
-            }
-        }
-        scrollPos = currentTop;
+        });
+    }
+
+    all_posts.sort(function(post_a, post_b){
+        date_a = new Date(post_a.month + " " + post_a.date + "," + post_a.year)
+        date_b = new Date(post_b.month + " " + post_b.date + "," + post_b.year)
+        return date_b - date_a;
     });
-})
+
+    for (let i = 0; i < all_posts.length; i++) {
+            // console.log(all_posts[i])
+            $( "#btn_more").before(`
+                <!-- Post preview-->
+                <div class="post-preview">
+                    <a href=` + all_posts[i].url + `>
+                        <h2 class="post-title">` + all_posts[i].title + `</h2>
+                        <h3 class="post-subtitle">` + all_posts[i].subtitle + `</h3>
+                    </a>
+                    <p class="post-meta">
+                        Posted by
+                        <a href="https://wuhanstudio.cc">` + all_posts[i].author + `</a>
+                        on ` + all_posts[i].month + ' ' + all_posts[i].date + `, ` + all_posts[i].year + `
+                    </p>
+                </div>
+                <!-- Divider-->
+                <hr class="my-4" />
+            `);
+    }
+}
